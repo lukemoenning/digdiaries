bb6ec597a91077451faf0bbcd5c09354b1903878
lukemoenning <luke.moenning@gmail.com>
2023-06-30 10:25:13 -0500

    remove test post

--- app/components/InDevelopmentAlert.tsx
+++ app/components/InDevelopmentAlert.tsx
@@ -1,11 +1,13 @@
-import { NormalPageWidth, HeaderText } from '@/app/libs/common-components'
+import { NormalPageWidth, BodyText } from '@/app/libs/common-components'
+import { theme } from '@/app/libs/theme'
+import { NextLink }from '@/app/libs/common-components'
 
 function InDevelopmentAlert() {
   return (
     <NormalPageWidth>
-      <HeaderText style={{margin: 'auto'}}>
-        This page is currently in development. Check back in later!
-      </HeaderText>
+      <BodyText style={{margin: 'auto'}}>
+        This page is currently in development. Check out my <NextLink style={{color: `${theme.colors.lightGreen}`, textAlign: 'center'}} href={`/blog/`}>blogs</NextLink> in the meantime!
+      </BodyText>
     </NormalPageWidth>
   )
 }
